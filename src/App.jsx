bf44aaf77d2e3e118875aa710e3f93d232900854
m.zakharov <m.zakharov@dunice.net>
2021-11-11 09:31:47 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [water, setWater] = useState(0);
-  const [TIMER_ID, setTIMER_ID] = useState(0);
-
-  const change = (isUp) => {
-    clearTimeout(TIMER_ID);
-    const id = setInterval(() => {
-        setWater((prevState) => { return isUp ? prevState + 20 : prevState - 20 });
-    }, 2000)
-    setTIMER_ID(id);
-  }
+  const [isUp, setIsUp] = useState();
 
   useEffect(() => {
-    if ((water >= 100) || (water <= 0))
-      clearTimeout(TIMER_ID);
-  }, [water])
+   const interval = setInterval(() => {
+      if (isUp === 'increase' && water < 100) {
+        setWater((prevState) => prevState + 20);
+      }
+      if (isUp === 'decrease' && water > 0) {
+        setWater((prevState) => prevState - 20);
+      }}, 2000)
+      return () => clearInterval(interval); 
+  }, [water, isUp])
 
   return (
     <div className="App">
       <div className="App-buttons">
-        <button disabled={water === 100} onClick={() => change(true)}> increaseWaterLevel </button>
-        <button disabled={water === 0} onClick={() => change(false)}> decreaseWaterLevel </button>
+        <button disabled={water === 100} onClick={() => {setIsUp('increase')}}> increaseWaterLevel </button>
+        <button disabled={water === 0} onClick={() => {setIsUp('decrease')}}> decreaseWaterLevel </button>
       </div>
       <span className='bathtub-description'>Water Level: {water}%</span>
       <div className='bathtub'>
